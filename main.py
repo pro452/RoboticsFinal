84ed75a1a76228ba4ddef5a8c0e5019975158299
Michael Matthews <MM@michaels-mbp-2.rowanads.rowan.edu>
2018-12-07 18:08:11 -0500

    Added functions for flow

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-map = [1,1,0,1,0,0,0,0,1]
+map = [1,1,0,
+       1,0,0,
+       0,0,1]
 
 probaility = []
 
 for i in range(len(map)):
     probaility.append(1.0/len(map))
+
+def possibile_locations():
+    #input the result from the 3 ultrasonic sensors
+    #process: //This is the hard part
+    #output possible locations of the robot based upon, the 3 ultrasonic senors
+
+def update_stat():
+    #input: the output of possible_locations
+    #process: using Probabilistic locationlization, update where the robot could be
+    #output an updated probablity array
+
+def path_finder():
+    #input: proablitity array
+    #process: using highest probability from the probliity array what is the shortest path to the goal
+    #output: Where should robot go next
+
+
+
