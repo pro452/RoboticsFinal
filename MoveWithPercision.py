69cc8b40ff9a6234e1cfe4ce01d86070e3c6e628
Matthew Chabalowski <chabalowm1@students.rowan.edu>
2018-12-11 17:08:58 -0500

    updated the movement functions, so that they include the color sensor input

--- MoveWithPercision.py
+++ MoveWithPercision.py
@@ -3,19 +3,35 @@
 from ev3dev.ev3 import *
 from time import sleep
 
-lm = LargeMotor('outB')
-rm = LargeMotor('outC')
+#initialize motors
+leftMoto = LargeMotor('outB')
+rightMoto = LargeMotor('outC')
+
+#initialize color sensor
+colSen = ColorSensor('in4')
 
 def moveStraightWithPercision() :
     #move straight roughly 7.5 inches
-    rm.run_to_rel_pos(position_sp=360, speed_sp=100, stop_action="hold")
-    lm.run_to_rel_pos(position_sp=360, speed_sp=100, stop_action="hold")
+    rightMoto.run_to_rel_pos(position_sp=360, speed_sp=100, stop_action="hold")
+    leftMoto.run_to_rel_pos(position_sp=360, speed_sp=100, stop_action="hold")
     sleep(5)
+    
 def turnRightWithPercision() :
-    #quarter turn right
-    rm.run_to_rel_pos(position_sp=-180, speed_sp=100, stop_action="hold")
-    lm.run_to_rel_pos(position_sp=180, speed_sp=100, stop_action="hold")
+    #half quarter turn right to cross the N/S line
+    rightMoto.run_to_rel_pos(position_sp=-180, speed_sp=100, stop_action="hold")
+    leftMoto.run_to_rel_pos(position_sp=180, speed_sp=100, stop_action="hold")
+    
+    #While the sensor is not on the W/E line, turn right stop when the sensor detects the line
+        while colSen.Value() > 50 :
+        rightMoto.run_to_rel_pos(position_sp=-180, speed_sp=100, stop_action="hold")
+        leftMoto.run_to_rel_pos(position_sp=180, speed_sp=100, stop_action="hold")
+
 def turnLeftWithPercision() :
-    #quarter turn right
-    rm.run_to_rel_pos(position_sp=-180, speed_sp=100, stop_action="hold")
-    lm.run_to_rel_pos(position_sp=180, speed_sp=100, stop_action="hold")
+    #half quarter turn left to cross the N/S line
+    rightMoto.run_to_rel_pos(position_sp=180, speed_sp=100, stop_action="hold")
+    leftMoto.run_to_rel_pos(position_sp=-180, speed_sp=100, stop_action="hold")
+    
+    #While the sensor is not on the W/E line, turn left stop when the sensor detects the line
+        while colSen.Value() > 50 :
+        rightMoto.run_to_rel_pos(position_sp=180, speed_sp=100, stop_action="hold")
+        leftMoto.run_to_rel_pos(position_sp=-180, speed_sp=100, stop_action="hold")
