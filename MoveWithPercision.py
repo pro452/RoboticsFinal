ca3b60d5ffb3055ec41a8c51c6bdc597dc0fc46f
Michael Naples <37196117+MNaplesDevelopment@users.noreply.github.com>
2018-12-17 17:36:46 -0500

    Update MoveWithPercision.py

--- MoveWithPercision.py
+++ MoveWithPercision.py
@@ -10,6 +10,29 @@ rightMoto = LargeMotor('outC')
 #initialize color sensor
 colSen = ColorSensor('in4')
 
+def find_line():
+    rightMoto.move_to_rel_pos(position_sp=50, speed_sp=50, stop_action='brake')
+    while rightMoto.is_running:
+        if colSen.value() < 12:
+            rightMoto.stop(stop_action='brake')
+            return
+    leftMoto.move_to_rel_pos(position_sp=100, speed_sp=50, stop_action='brake')
+    while leftMoto.is_running:
+        if colSen.value() < 12:
+            leftMoto.stop(stop_action='brake')
+            break
+
+def move_foward():
+    rightMoto.move_to_rel_pos(position_sp=200, speed_sp=100, stop_action='brake')
+    leftMoto.move_to_rel_pos(position_sp=200, speed_sp=100, stop_action='brake')
+
+    while rm.is_running:
+        if colSen.value() > 30 and colSen.value() < 70:
+            rightMoto.stop(stop_action='brake')
+            leftMoto.stop(stop_action='brake')
+            find_line()
+            break
+
 def moveStraightWithPercision() :
     #move straight roughly 7.5 inches
     rightMoto.run_to_rel_pos(position_sp=360, speed_sp=100, stop_action="hold")
