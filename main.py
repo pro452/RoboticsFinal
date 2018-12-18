726a9dfd43ed2099465114efbe7ffa84779b2627
Michael Matthews <MM@michaels-mbp-2.rowanads.rowan.edu>
2018-12-17 19:00:46 -0500

    cleaning up the code

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
+#This file was written entirely by Michael Matthews
+
 map = [[9,9,9,9,9,9,9,9],
-       [9,1,0,1,0,1,1,9], # zero representing no obstacle and one representing an obstacle and 9 representing the edge of the obstacle course
+       [9,1,0,1,0,1,1,9], # zero representing no obstacle and one representing an obstacle and nine representing the edge of the obstacle course
        [9,1,0,0,0,1,1,9],
        [9,1,1,0,1,1,1,9],
        [9,1,1,0,0,1,1,9],
        [9,9,9,9,9,9,9,9]]
 
 probaility = []
-
 orientation = 'east'
 
 for i in range(8): #Initilize
     probaility.append(1.0/8)
 
+#This function determines where the robot could be base upon its sensors detecting wether or not an obstacle is next to it or not
 def possibile_locations(Left, Front, Right):
     probs = []
     for x in range(1,5):
         for y in range(1,7):
-            #print(map[x][y])
-            #print(orientation)
             if map[x][y] == 0:
                 if orientation == 'north':
                     if (map[x][y-1] == Left or (map[x][y-1] == 9 and Left != 0)) and (map[x-1][y] == Front or (map[x-1][y] == 9 and Front != 0)) and (map[x][y+1] == Right or (map[x][y+1] == 9 and Right != 0)):
@@ -33,10 +33,7 @@ def possibile_locations(Left, Front, Right):
                         probs.append(str(x)+str(y)+'w')
     return probs
 
-    #input the result from the 3 ultrasonic sensors
-    #process: //This is the hard part
-    #output possible locations of the robot based upon, the 3 ultrasonic senors
-
+#This function updates the probablistic model so that it can approximate where it is currently located
 def update_stat(locations):
     locations_to_add_more_probability = []
 
@@ -58,8 +55,7 @@ def update_stat(locations):
         if locations[index][0] == '4' and locations[index][1] == '4':
             locations_to_add_more_probability.append(7)
 
-
-    for i in range(len(locations_to_add_more_probability)):  # Initilize
+    for i in range(len(locations_to_add_more_probability)):
         probaility[locations_to_add_more_probability[i]] += 1.0/len(locations_to_add_more_probability)
 
     sum = 0
@@ -70,16 +66,11 @@ def update_stat(locations):
     for index in range(len(probaility)):
         probaility[index] = probaility[index]/sum
 
-    #print(probaility.index(max(probaility)))
-    print(probaility)
     return locations_to_add_more_probability
-    #input: the output of possible_locations
-    #process: using Probabilistic locationlization, update where the robot could be
-    #output an updated probablity array
 
+#This function finds the path to the goal based upon where the robot thinks its located and tells the robot which direction to go to
 def path_finder(start_location):
 
-    #start_location = "320"
     counter = 0
     secondmap=[[9,9,9,9,9,9,9,9],
                [9,-1, 0,-1, 5,-1,-1,9],
@@ -88,7 +79,6 @@ def path_finder(start_location):
                [9,-1,-1, 0, 0,-1,-1,9],
                [9,9,9,9,9,9,9,9]]
 
-    #path_map[start_location] = counter
     list_of_nodes = []
     list_of_nodes.append(start_location)
 
@@ -106,52 +96,34 @@ def path_finder(start_location):
                     list_of_nodes.append(str(x) + str(y-1) + str(counter + 1))
                     if secondmap[x][y-1] == 5:
                         goalFound = True
-                        print(str(x) + ' ' + str(y) + str(counter))
                     secondmap[x][y - 1] = -1
                 if secondmap[x][y+1] == 0 or secondmap[x][y+1] == 5:
                     list_of_nodes.append(str(x) + str(y+1) + str(counter + 1))
                     if secondmap[x][y+1] == 5:
                         goalFound = True
-                        print(str(x) + ' ' + str(y))
                     secondmap[x][y + 1] = -1
                 if secondmap[x-1][y] == 0 or secondmap[x-1][y] == 5:
                     list_of_nodes.append(str(x-1) + str(y) + str(counter + 1))
                     if secondmap[x-1][y] == 5:
                         goalFound = True
-                        print(str(x) + ' ' + str(y))
                     secondmap[x - 1][y] = -1
                 if secondmap[x+1][y] == 0 or secondmap[x+1][y] == 5:
                     list_of_nodes.append(str(x+1) + str(y) + str(counter + 1))
                     if secondmap[x+1][y] == 5:
                         goalFound = True
-                        print(str(x) + ' ' + str(y))
                     secondmap[x + 1][y] = -1
 
         counter += 1
 
-    print(list_of_nodes)
-    print(str(counter))
-    #This loop finds where the goal is
-    # max = 0
     indexOfmaxvalue = -1
-    # for index in range(len(list_of_nodes)):
-    #     if int(list_of_nodes[index][2]) > int(max):
-    #         indexOfmaxvalue = index
-    #         max = list_of_nodes[index][2]
 
     pathNeededToTravel = []
-    #pathNeededToTravel.append(list_of_nodes[indexOfmaxvalue])
-    pathNeededToTravel.append("14" + str(counter))
+
+    pathNeededToTravel.append("14" + str(counter)) #Where the goal is located
+
     for index in range(len(list_of_nodes)):
         if int(list_of_nodes[index][0]) == 1 and int(list_of_nodes[index][1]) == 4:
            indexOfmaxvalue = index
-           print("max value " + str(list_of_nodes[indexOfmaxvalue]))
-
-    #print(list_of_nodes) # This may be the one
-    # print(list_of_nodes[indexOfmaxvalue][1])
-    # print(int(list_of_nodes[0][1]) - 1)
-    # print(list_of_nodes[indexOfmaxvalue][2])
-    # print(int(list_of_nodes[0][2]) + 1)
 
     #This loop creates the path needed for the robot
     while counter > 0:
@@ -167,65 +139,49 @@ def path_finder(start_location):
                 pathNeededToTravel.append(list_of_nodes[index])
                 indexOfmaxvalue = index
                 counter = counter - 1
-                print(str(indexX) + str(indexY) + '1')
-                print(str(maxValueX) + str(maxValueY) + '1max')
-
                 break
             if maxValueX == indexX and maxValueY == indexY + 1 and maxValueCounter == indexCounter:
                 pathNeededToTravel.append(list_of_nodes[index])
                 indexOfmaxvalue = index
                 counter = counter - 1
-                print(str(indexX) + str(indexY) +'2')
-                print(str(maxValueX) + str(maxValueY) + '2max')
-
                 break
             if maxValueX == indexX + 1 and maxValueY == indexY and maxValueCounter == indexCounter:
                 pathNeededToTravel.append(list_of_nodes[index])
                 indexOfmaxvalue = index
                 counter = counter - 1
-                print(str(indexX) + str(indexY)+'3')
-                print(str(maxValueX) + str(maxValueY) + '3max')
-
                 break
             if maxValueX == indexX - 1 and maxValueY == indexY and maxValueCounter == indexCounter:
                 pathNeededToTravel.append(list_of_nodes[index])
                 indexOfmaxvalue = index
                 counter = counter - 1
-                print(str(indexX) + str(indexY)+'4')
-                print(str(maxValueX) + str(maxValueY) + '4max')
                 break
-
-    print(pathNeededToTravel)
 
     #This will figure out what to tell the robot to do next
     xtravel = int(pathNeededToTravel[len(pathNeededToTravel) - 2][0]) - int(pathNeededToTravel[len(pathNeededToTravel) - 1][0])
     ytravel = int(pathNeededToTravel[len(pathNeededToTravel) - 2][1]) - int(pathNeededToTravel[len(pathNeededToTravel) - 1][1])
 
     if ytravel == 1:
-        print("EAST")
+        return "EAST"
     if ytravel == -1:
-        print("WEST")
+        return "WEST"
     if xtravel == 1:
-        print("SOUTH")
+        return "SOUTH"
     if xtravel == -1:
-        print("NORTH")
+        return "NORTH"
 
-    #input: proablitity array
-    #process: using highest probability from the probliity array what is the shortest path to the goal
-    #output: Where should robot go next
+listOfPrevSteps = []
 
 while 1 == 1:
     orientation = input("Orientation: ")
     east = input("East: ")
     north = input("North: ")
     west = input("West: ")
+    steps = 0
 
     locations = possibile_locations(int(east),int(north),int(west))
-    print(locations)
-
     highestProbLocation = update_stat(locations)
-    print(highestProbLocation)
 
+    #This function figures out where the robot is located
     def findStartlocation():
         start_location = -1
         if highestProbLocation[0] == 0:
@@ -248,9 +204,9 @@ while 1 == 1:
         return start_location
 
     start_location = findStartlocation()
-    #start_location = input("Highest prob. location: ")
-    print(start_location)
-    path_finder(str(start_location))
+    whichDirectionToMove = path_finder(str(start_location))
+    listOfPrevSteps.append(whichDirectionToMove)
+    print(whichDirectionToMove)
     remove = input("Remove? ")
 
     if remove == 'yes':
